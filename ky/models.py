169d469474ae5d640ea306c82dac3e4fabd21f81
Jusung Lee <js100810081@gmail.com>
2025-01-13 11:30:11 +0900

    불필요한 폴더 삭제

--- ky/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Boolean, Column, String
-from .database import Base
-
-class UserModel(Base):
-    __tablename__ = "users"
-
-    username = Column(String, primary_key=True, index=True)
-    hashed_password = Column(String)
-    disabled = Column(Boolean, default=False)
